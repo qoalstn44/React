1967112e470a8850816aa5fab66a78b928157950
qoalstn44 <qoalstn44@naver.com>
2022-12-05 10:13:43 +0900

    Create React APP

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>스파르타 코딩 리엑스 강의 시작!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
