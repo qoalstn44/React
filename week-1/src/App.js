f45a71a6fb6b2e761f6e73811fe1e34c62816f7c
qoalstn44 <qoalstn44@naver.com>
2022-12-05 10:14:19 +0900

    yarn start!

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>스파르타 코딩 리엑스 강의 시작!</p>
+        <p>스파르타 코딩 리엑스 강의 시작!!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
