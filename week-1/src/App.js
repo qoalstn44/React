ab86461d2fec1f6c4b3db4a0ff661f7db5917b3f
qoalstn44 <qoalstn44@naver.com>
2022-12-06 19:07:19 +0900

    use_state

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,12 +1,35 @@
-// components/Child.js
+// src/App.js
 
-import React from "react";
+import React, { useState } from "react";
 
-function Child({ name = "기본이름" }) {
-  return <div>내 이름은 {name} 입니다. </div>; //defaultProps 사용예시
+function Child(props) {
+  return (
+    <div>
+      <button
+        onClick={() => {
+          props.setName("배민수"); // 드디어 받은 setName을 실행합니다.
+        }}
+      >
+        할아버지 이름 바꾸기
+      </button>
+      <div>{props.grandFatherName}</div>
+    </div>
+  );
 }
 
-// Child.defaultProps = {
-//   name: "기본이름",
-// }; //defaultProps 사용예시
-export default Child;
+function Mother(props) {
+  return (
+    <Child grandFatherName={props.grandFatherName} setName={props.setName} /> // 받아서 다시 주고
+  );
+}
+
+function GrandFather() {
+  const [name, setName] = useState("김할아");
+  return <Mother grandFatherName={name} setName={setName} />; // 주고
+}
+
+function App() {
+  return <GrandFather />;
+}
+
+export default App;
