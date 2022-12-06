a5ed2bb95562d6b667d66c0477856f1aafc5015f
qoalstn44 <qoalstn44@naver.com>
2022-12-06 11:41:15 +0900

    component

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,24 +1,20 @@
+// src/App.js
 import React from "react";
+
+function Child() {
+  return <div>Child</div>;
+}
+
+function Mother() {
+  return <Child />;
+}
+
+function GrandFather() {
+  return <Mother />;
+}
+
 function App() {
-  // <---- 자바스크립트 영역 ---->
-  const handClike = () => {
-    alert("클릭!");
-  };
-  return (
-    /* <---- HTML/JSX 영역  ---->*/
-    <div
-      style={{
-        height: "100vh",
-        display: " flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <span>내가만든 APP 컨포넌트 입니다.</span>
-      <button onClick={handClike}>클릭!</button>
-    </div>
-  );
+  return <GrandFather />;
 }
 
 export default App;
