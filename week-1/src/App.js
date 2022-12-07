cba704f74197217ba5b0c729947cfddff0a86621
qoalstn44 <qoalstn44@naver.com>
2022-12-07 11:24:30 +0900

    useState

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,35 +1,18 @@
 // src/App.js
 
-import React, { useState } from "react";
-
-function Child(props) {
-  return (
-    <div>
-      <button
-        onClick={() => {
-          props.setName("배민수"); // 드디어 받은 setName을 실행합니다.
-        }}
-      >
-        할아버지 이름 바꾸기
-      </button>
-      <div>{props.grandFatherName}</div>
-    </div>
-  );
-}
-
-function Mother(props) {
-  return (
-    <Child grandFatherName={props.grandFatherName} setName={props.setName} /> // 받아서 다시 주고
-  );
-}
-
-function GrandFather() {
-  const [name, setName] = useState("김할아");
-  return <Mother grandFatherName={name} setName={setName} />; // 주고
-}
+import React from "react";
 
 function App() {
-  return <GrandFather />;
+  function onClick() {
+    setName("이몽룡");
+  }
+  const [name, setName] = React.useState("홍길동");
+  return (
+    <div>
+      {name}
+      <button onClick={onClick}>버튼</button>
+    </div>
+  );
 }
 
 export default App;
