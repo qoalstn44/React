977751639aa078e7bde9c08fd4683d0b3ea8ee4c
qoalstn44 <qoalstn44@naver.com>
2022-12-12 15:12:25 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
+// // src/App.js
+
+// import { useState } from "react";
+
+// const App = () => {
+//   const [number, setNumber] = useState(0);
+//   return (
+//     <div>
+//                         {/* 버튼을 누르면 1씩 플러스된다. */}
+//       <div>{number}</div>
+//       <button
+//         onClick={() => {
+//           setNumber(number + 1); // 첫번째 줄
+//           setNumber(number + 1); // 두번쨰 줄
+//           setNumber(number + 1); // 세번째 줄
+//         }}
+//       >
+//         버튼
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 // src/App.js
 
-import React from "react";
-import styled from "styled-components";
+import { useState } from "react";
 
-// 1. styled-components를 만들었습니다.
-const StBox = styled.div`
-  width: 100px;
-  height: 100px;
-  border: 1px solid ${(props) => props.borderColor}; // 4.부모 컴포넌트에서 보낸 props를 받아 사용합니다.
-  margin: 20px;
-`;
-const boxList = ["red", "green", "blue"];
-
-// 색을 넣으면, 이름을 반환해주는 함수를 만듭니다.
-const getBoxName = (color) => {
-  switch (color) {
-    case "red":
-      return "빨간 박스";
-    case "green":
-      return "초록 박스";
-    case "blue":
-      return "파란 박스";
-    default:
-      return "검정 박스";
-  }
-};
 const App = () => {
+  const [number, setNumber] = useState(0);
   return (
     <div>
-      {boxList.map((box) => (
-        <StBox borderColor={box}>{getBoxName(box)}</StBox>
-      ))}
+      {/* 버튼을 누르면 3씩 플러스 된다. */}
+      <div>{number}</div>
+      <button
+        onClick={() => {
+          setNumber((previousState) => previousState + 1);
+          setNumber((previousState) => previousState + 1);
+          setNumber((previousState) => previousState + 1);
+        }}
+      >
+        버튼
+      </button>
     </div>
   );
 };
