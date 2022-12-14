515f6d2b3df7ed4a8b88258586cf92fb375d473c
qoalstn44 <qoalstn44@naver.com>
2022-12-14 10:17:21 +0900

    useEffect hook

--- src/App.js
+++ src/App.js
@@ -25,25 +25,62 @@
 
 // src/App.js
 
-import { useState } from "react";
+//import { useState } from "react";
 
-const App = () => {
-  const [number, setNumber] = useState(0);
+//const App = () => {
+//const [number, setNumber] = useState(0);
+// return (
+//  <div>
+//    {/* 버튼을 누르면 3씩 플러스 된다. */}
+//     <div>{number}</div>
+//     <button
+//      onClick={() => {
+//       setNumber((previousState) => previousState + 1);
+//       setNumber((previousState) => previousState + 1);
+//        setNumber((previousState) => previousState + 1);
+//      }}
+//     >
+//       버튼
+//     </button>
+//    </div>
+// );
+//};
+
+//export default App;
+import { useState, useEffect } from "react";
+
+export default function FunctionComponent() {
+  const [color, setColor] = useState();
+
+  const changeColor = (color) => {
+    setColor(color);
+  };
+
+  useEffect(() => {
+    console.log("컴포넌트가 마운트 되었습니다.");
+  }, []);
+
+  useEffect(() => {
+    console.log("컴포넌트가 마운트 되었습니다.");
+  }, [color]);
+
   return (
     <div>
-      {/* 버튼을 누르면 3씩 플러스 된다. */}
-      <div>{number}</div>
-      <button
-        onClick={() => {
-          setNumber((previousState) => previousState + 1);
-          setNumber((previousState) => previousState + 1);
-          setNumber((previousState) => previousState + 1);
+      <div
+        style={{
+          width: "200px",
+          height: "100px",
+          margin: "auto",
+          backgroundColor: color,
+          border: "1px solid #ccc",
         }}
-      >
-        버튼
-      </button>
+      ></div>
+      <div>
+        <button onClick={() => changeColor("red")}>빨간색</button>
+        <button onClick={() => changeColor("blue")}>파란색</button>
+        <button onClick={() => changeColor("green")}>초록색</button>
+        <button onClick={() => changeColor("black")}>검은색</button>
+      </div>
     </div>
   );
-};
-
-export default App;
+}
